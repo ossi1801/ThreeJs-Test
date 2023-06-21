263c99cd33614c16565f5f6beee896c4916923bc
ossi1801 <ossihakki@gmail.com>
2023-06-21 21:13:35 +0300

    Fixed grab animation, added mouse1 listener for testing

--- scripts/scene.js
+++ scripts/scene.js
@@ -2,27 +2,25 @@ import * as THREE from '../three.js-master/build/three.module.js';
 import { GLTFLoader } from '../three.js-master/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from '../three.js-master/examples/jsm/controls/OrbitControls.js';
 export default function init() {
-    var renderer, scene, camera, controls;
+    window.addEventListener('click', playGrabAnim);
+    var renderer, scene, camera, controls, mixer, clock, model, action;
+    var endOfAnim = false;
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x889988);
     const container = document.querySelector('#threejs-container');
     container.append(renderer.domElement);
-
+    clock = new THREE.Clock(); //clock for anims
     scene = new THREE.Scene();
-
     camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.set(20, 20, 20);
     scene.add(camera); // required, since adding light as child of camera
-
-    // controls
-    controls = new OrbitControls(camera, renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);    // controls
     controls.maxPolarAngle = Math.PI / 2; //limit y-xis (can't roll to below scene)
 
     // ambient
     scene.add(new THREE.AmbientLight(0x444444));
-
     // light
     var light = new THREE.PointLight(0xffffff, 0.8);
     camera.add(light);
@@ -55,27 +53,14 @@ export default function init() {
     function loadGrab(url = '../models/grab.gltf') {
         // Load a glTF resource
         const loader = new GLTFLoader();
-        var mixer;
-        var action;
-        var scale = 1;
-        // change to url (github) for pages version (?)
         console.log(url);
         loader.load(url, function (gltf) {
-            console.log(gltf);
-
-            var object = gltf.scene;
-            object.scale.set(scale, scale, scale);
-            scene.add(object);
-
-            mixer = new THREE.AnimationMixer(object);
-            //mixer.timeScale = -1; for reverse
-            action = mixer.clipAction(gltf.animations[0]); //why no work
-
-            action.play();
-
-            // gltf.animations; // Array<THREE.AnimationClip>
-            // gltf.cameras; // Array<THREE.Camera>
-            // gltf.asset; // Object
+            model = gltf;
+            mixer = new THREE.AnimationMixer(model.scene);
+            action = mixer.clipAction(gltf.animations[0]);
+            action.setLoop(THREE.LoopOnce); // Do only once
+            action.clampWhenFinished = true; //Finishing pos
+            scene.add(model.scene);
 
             //if everything okay draw extra stuff
             //Line 
@@ -88,7 +73,6 @@ export default function init() {
             const lineGeo = new THREE.BufferGeometry().setFromPoints(points);
             const line = new THREE.Line(lineGeo, lineMat);
             scene.add(line);
-            action.play();
         },
             function (xhr) {  // called while loading is progressing
                 console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -101,7 +85,8 @@ export default function init() {
     }
     function animate() {
         requestAnimationFrame(animate);
-        //controls.update();
+        var delta = clock.getDelta();
+        if (mixer) mixer.update(delta);
         //const rndInt = randomIntFromInterval(1, 6)
         //line.rotation.x += 0.01;
         //line.rotation.y += 0.01;
@@ -110,9 +95,27 @@ export default function init() {
     }
     function randomIntFromInterval(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
+    } 
+    function playGrabAnim() {
+        action = mixer.clipAction(model.animations[0]);
+        action.paused = false;
+        action.setLoop(THREE.LoopOnce);
+        if (endOfAnim) {
+            if (action.time === 0) {
+                action.time = action.getClip().duration;
+            }
+            action.timeScale = -1;
+            //action.play();
+        } else {
+            action.timeScale = 1;
+        }
+        endOfAnim = !endOfAnim;
+        if (action !== null) {
+            //action.stop();
+            action.play();
+
+        }
     }
-
-
     function createBox(width = 2, height = 2, depth = 2, color = 'gray', x = -2, y = -4, z = -6) {
         let geometry = new THREE.BoxGeometry(width, height, depth);
         let material1 = new THREE.MeshPhongMaterial({
